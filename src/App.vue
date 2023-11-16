8d57d1f9513aa86ea6ebc2246dbfe750ecdc9603
Jorge <91501563+Jrge182@users.noreply.github.com>
2023-11-15 23:58:25 -0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <UserCard
+      userName="Jorge Enrique"
+      userEmail="jorgee@example.com"
+      userBio="Ing. En Sistemas De Información"
+    />
+  </div>
+
+  <div id="app2">
+    <UserCard
+      userName="Jorge Enrique 2"
+      userEmail="jorgee2@example.com"
+      userBio="Ing. En Sistemas De Información"
+    />
+  </div>
+</template>
+
+<script>
+import UserCard from "./UserCard.vue";
+
+
+export default {
+  components: {
+    UserCard,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #000;
+  margin-top: 60px;
+}
+</style>
---
+++ src/UserCard.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="user-card">
+    <div class="user-avatar">
+    <img src="https://i.ibb.co/gMGHDnz/imagen-2023-10-13-163835566.png"
+  style="width: 50px;
+        height: 50px;
+        border-radius: 50%;
+        background-color: #ddd;
+        margin-right: 15px;">
+
+    </div>
+    <div class="user-info">
+      <h2>{{ userName }}</h2>
+      <p>{{ userEmail }}</p>
+      <p>{{ userBio }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    userName: {
+      type: String,
+      required: true,
+    },
+    userName2: {
+      type: String,
+      required: true,
+    },
+    userEmail: {
+      type: String,
+      required: true,
+    },
+    userEmail2: {
+      type: String,
+      required: true,
+    },
+    userBio: {
+      type: String,
+      required: true,
+    },
+    userBio2: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.user-card {
+  border: 1px solid #ddd;
+  padding: 15px;
+  margin: 10px;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  display: flex;
+  align-items: center;
+}
+
+
+.user-info {
+  flex-grow: 1;
+}
+
+.user-info h2 {
+  margin: 0;
+}
+
+.user-info p {
+  margin: 5px 0;
+}
+
+.user-info2 {
+  flex-grow: 1;
+}
+
+.user-info2 h2 {
+  margin: 0;
+}
+
+.user-info2 p {
+  margin: 5px 0;
+}
+</style>
